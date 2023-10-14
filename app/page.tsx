d744a43e730bdc3eb31e35f471b16577d4b493fb
Henrique <henriquebzn1@gmail.com>
2023-10-14 15:56:08 -0300

    Implemented responsive properties

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import ShotClock from '@/components/ShotClock'
 
 const Home: FC = () => {
   return (
-    <main className="flex flex-col items-center min-h-screen bg-black  text-white">
+    <main className="flex flex-col items-center min-h-screen bg-black  text-white px-[3%] pb-16">
       <h1 className="text-8xl mt-20 font-bold text-center -mb-10">
         React Shot Clock 🏀
       </h1>
 
-      <div className="flex justify-center gap-28">
+      <div className="flex flex-col items-center lg:items-start lg:flex-row lg:justify-center gap-10 lg:gap-28">
         <ShotClock />
         <ShotClock />
       </div>
