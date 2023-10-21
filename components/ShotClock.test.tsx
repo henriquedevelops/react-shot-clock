f62c2c645d3884d687574469b547528ede3e946a
Henrique <henriquebzn1@gmail.com>
2023-10-21 15:28:34 -0300

    Finished unit tests defined on last commit

--- components/ShotClock.test.tsx
+++ components/ShotClock.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, act } from '@testing-library/react'
 import ShotClock from './ShotClock'
+
+jest.useFakeTimers()
 
 test('ShotClock renders without errors', () => {
   render(<ShotClock />)
@@ -10,25 +12,57 @@ test('ShotClock starts counting when "Start" is clicked', () => {
   const { getByTestId } = render(<ShotClock />)
   const startOrStopButton = getByTestId('start-stop-button')
 
+  expect(getByTestId('status-text').textContent).toBe('Stopped')
+
   fireEvent.click(startOrStopButton)
+
+  expect(getByTestId('status-text').textContent).toBe('Counting...')
 })
 
 test('ShotClock stops counting when "Stop" is clicked', () => {
   const { getByTestId } = render(<ShotClock />)
   const startOrStopButton = getByTestId('start-stop-button')
 
+  expect(getByTestId('status-text').textContent).toBe('Stopped')
+
   fireEvent.click(startOrStopButton)
 
-  const stopButton = getByTestId('start-stop-button')
-  fireEvent.click(stopButton)
+  expect(getByTestId('status-text').textContent).toBe('Counting...')
+
+  fireEvent.click(startOrStopButton)
+
+  expect(getByTestId('status-text').textContent).toBe('Stopped')
+})
+
+test('ShotClock counts down correctly', () => {
+  const { getByTestId } = render(<ShotClock />)
+  const startOrStopButton = getByTestId('start-stop-button')
+  const countdownDisplay = getByTestId('countdown-display')
+
+  fireEvent.click(startOrStopButton)
+
+  act(() => {
+    jest.advanceTimersByTime(1000)
+  })
+
+  expect(countdownDisplay.textContent).toBe('23')
 })
 
 test('ShotClock resets when "Reset" is clicked', () => {
   const { getByTestId } = render(<ShotClock />)
   const startOrStopButton = getByTestId('start-stop-button')
+  const resetButton = getByTestId('reset-button')
+  const countdownDisplay = getByTestId('countdown-display')
 
   fireEvent.click(startOrStopButton)
 
-  const resetButton = getByTestId('reset-button')
+  act(() => {
+    jest.advanceTimersByTime(1000)
+  })
+
+  expect(countdownDisplay.textContent).toBe('23')
+
   fireEvent.click(resetButton)
+
+  expect(countdownDisplay.textContent).toBe('24')
 })
